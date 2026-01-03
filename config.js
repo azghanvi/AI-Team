d62ef459bf63315e78ef0f856b47a6cb5ef57d8d
azghanvi <azghanvi@gmail.com>
2026-01-04 00:59:52 +0500

    config

---
+++ config.js
@@ -0,0 +1,6 @@
+// update your GROQ Keys here: console.groq.com
+// Better to use different keys from different accnount to omit rate limit
+
+const GROQ_KEY1 = '';
+const GROQ_KEY2 = '';
+const GROQ_KEY3 = '';
